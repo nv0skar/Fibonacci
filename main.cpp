0dacc237d9ca97cc0a5f96de729f823df19713f1
ItsTheGuy <36568572+ItsTheGuy@users.noreply.github.com>
2022-07-04 17:01:32 +0200

    Changed license banner

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // Fibonacci sequence calculator
-// Copyright (C) 2021 ItsTheGuy
+// Copyright (C) 2022 Oscar
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
